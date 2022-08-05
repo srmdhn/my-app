4cb514c3c3c5999a71756b5cd39cc94bc8ccf40d
Suci ramadhan <suci21005@gmail.com>
2022-08-05 11:06:40 +0700

    first commit

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {FiInstagram} from 'react-icons/fi'
+import {AiOutlineMail} from 'react-icons/ai'
+
+const Headersocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href="https://instagram.com"target="_blank"><FiInstagram/></a>
+      <a href="https://instagram.com"target="_blank"><AiOutlineMail/></a>
+    </div>
+  )
+}
+export default Headersocials
