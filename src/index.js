c8314a39854a2792c27d7dffda231a83167e4b1d
Suci ramadhan <suci21005@gmail.com>
2022-08-05 14:59:56 +0700

    Common fixed

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
